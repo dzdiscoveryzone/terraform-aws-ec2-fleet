6ea035ee01ca903cf180454133d58418741e5879
Dzenan Dervisevic <ddervise@gmail.com>
2020-01-09 23:13:42 -0500

    restructured folders to now have a live folder with current deployments and moved tf-remote-state into a global folder as you cannot use interpolation with backend config

--- live/dev/main.tf
+++ live/dev/main.tf
@@ -16,7 +16,7 @@ terraform {
 }
 
 module "vpc" {
-  source               = "../modules/vpc"
+  source               = "../../modules/vpc"
   cidr_block           = "172.24.0.0/16"
   availability_zones   = ["us-east-1a", "us-east-1b", "us-east-1c"]
   has_multiple_subnets = true
