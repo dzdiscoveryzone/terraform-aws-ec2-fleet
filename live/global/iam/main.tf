ebc32dedbd961bcf17a402374e1aed93dd0d492a
Dzenan Dervisevic <ddervise@gmail.com>
2020-09-24 16:25:03 -0400

    removed empty line from the top

--- live/global/iam/main.tf
+++ live/global/iam/main.tf
@@ -1,3 +1,23 @@
+resource "aws_iam_role" "tf_deployer" {
+  count = var.iam_role_count
+
+  name               = var.iam_role_name
+  assume_role_policy = data.aws_iam_policy_document.parent_account.json
+}
+
+resource "aws_iam_policy" "this" {
+  name        = var.iam_policy_name
+  description = "A policy to allow terraform to deploy and manage EC2 Instance, VPC's, S3 buckets, and DynamoDB Tables"
+  policy      = data.aws_iam_policy_document.tf_deploy.json
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  count = length(aws_iam_role.tf_deployer)
+
+  role       = aws_iam_role.tf_deployer[count.index].name
+  policy_arn = aws_iam_policy.this.arn
+}
+
 data "aws_iam_policy_document" "parent_account" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -32,24 +52,4 @@ data "aws_iam_policy_document" "tf_deploy" {
 
     resources = var.dynamodb_tables
   }
-}
-
-resource "aws_iam_role" "tf_deployer" {
-  count = var.iam_role_count
-
-  name               = var.iam_role_name
-  assume_role_policy = data.aws_iam_policy_document.parent_account.json
-}
-
-resource "aws_iam_policy" "this" {
-  name        = var.iam_policy_name
-  description = "A policy to allow terraform to deploy and manage EC2 Instance, VPC's, S3 buckets, and DynamoDB Tables"
-  policy      = data.aws_iam_policy_document.tf_deploy.json
-}
-
-resource "aws_iam_role_policy_attachment" "this" {
-  count = length(aws_iam_role.tf_deployer)
-
-  role       = aws_iam_role.tf_deployer[count.index].name
-  policy_arn = aws_iam_policy.this.arn
 }
